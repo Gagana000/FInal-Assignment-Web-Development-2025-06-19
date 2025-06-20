f38e0a55f212e9caf64fb311d0fef5260d92a09b
Gagana <152527577+Gagana000@users.noreply.github.com>
2025-06-20 13:27:16 +0530

    Creating Basic Nav bar

--- index.php
+++ index.php
@@ -3,15 +3,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home - NSBM Shop</title>
-    <!-- Bootstrap 5 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Bootstrap Icons -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.0/font/bootstrap-icons.css">
-    <!-- Custom CSS -->
+    <title>Home - NSBM Premium</title>
+    <!-- CSS Link -->
     <link rel="stylesheet" href="./style.css">
+    <!-- Inter Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
 </head>
 <body>
-    
+
+    <!----------------------------------------- NAVIGATION BAR ----------------------------------------->
+    <nav>
+        <div class="left-section">
+            <h1 class="Logo">NSBM Premium.</h1>
+        </div>
+        <div class="middle-section">
+            <div class="nav-buttons">
+                <ul>
+                    <li><a href="#"><i class="fa-solid fa-house"></i>Home</a></li>
+                    <li><a href="#"><i class="fa-solid fa-store"></i>Collection</a></li>
+                    <li><a href="#">Categories<i class="fa-solid fa-caret-down"></i></a>
+                        <ul>
+                            <li><a href="#">T-Shirts</a></li>
+                            <li><a href="#">Hoodies</a></li>
+                            <li><a href="#">Accessories</a></li>
+                            <li><a href="#">View All</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#"><i class="fa-solid fa-users"></i>About</a></li>
+                </ul>
+            </div>
+            <div class="nav-search">
+                <input type="text" name="search" id="search">
+            </div>
+        </div>
+        <div class="right-section">
+            <button type="button">Login<i class="fa-solid fa-user"></i></button>
+            <button type="button">Register<i class="fa-solid fa-user-plus"></i></button>
+            <button type="button"><i class="fa-solid fa-cart-shopping"></i></button>
+            <button type="button"><i class="fa-solid fa-user"></i></button>
+        </div>
+    </nav>
+
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/cb8b70f796.js" crossorigin="anonymous"></script>
 </body>
 </html>
