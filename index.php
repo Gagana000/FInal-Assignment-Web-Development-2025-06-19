533d0a2cecc629ba2cfc08af2425d6ae2a3b68d7
Gagana <152527577+Gagana000@users.noreply.github.com>
2025-06-21 22:03:24 +0530

    i just finished creating navigation bar and started to create the hero secrtion

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <!-- JavaScript Link -->
+    <script src="./main.js" defer></script>
 </head>
 <body>
 
@@ -18,25 +20,33 @@
         <div class="left-section">
             <h1 class="Logo">NSBM Premium.</h1>
         </div>
+
+        <div class="mobile-toggle">
+            <i class="fa-solid fa-bars"></i>
+        </div>
+
         <div class="middle-section">
             <div class="nav-buttons">
                 <ul>
                     <li><a href="#"><i class="fa-solid fa-house"></i>Home</a></li>
                     <li><a href="#"><i class="fa-solid fa-store"></i>Collection</a></li>
                     <li><a href="#">Categories<i class="fa-solid fa-caret-down"></i></a>
-                        <ul>
+                        <ul class="drop-down-menu">
                             <li><a href="#">T-Shirts</a></li>
                             <li><a href="#">Hoodies</a></li>
                             <li><a href="#">Accessories</a></li>
-                            <li><a href="#">View All</a></li>
+                            <li class="devider"></li>
+                            <li><a href="#" class="view-all">View All</a></li>
                         </ul>
                     </li>
                     <li><a href="#"><i class="fa-solid fa-users"></i>About</a></li>
                 </ul>
             </div>
+
             <div class="nav-search">
-                <input type="text" name="search" id="search">
+                <input type="text" name="search" id="search" placeholder="Search">
             </div>
+
         </div>
         <div class="right-section">
             <button type="button">Login<i class="fa-solid fa-user"></i></button>
@@ -46,7 +56,22 @@
         </div>
     </nav>
 
-    <!-- Font Awesome Icons -->
+    <!----------------------------------------- HERO SECTION ----------------------------------------->
+    <section class="hero">
+        <div class="hero-content">
+            <h1>Discover Exclusive NSBM Merchandise</h1>
+            <p>Premium quality apparel and accessories designed for the NSBM community</p>
+            <div class="hero-buttons">
+                <a href="#" class="btn-primary">Shop Now</a>
+                <a href="#" class="btn-outline">Learn More</a>
+            </div>
+        </div>
+        <div class="hero-image">
+            <img src="./assets/images/shopping_image.svg" alt="NSBM Merchandise">
+        </div>
+    </section>
+
+    <!--------------------------------------- FONT AWESOME ICONS --------------------------------------->
     <script src="https://kit.fontawesome.com/cb8b70f796.js" crossorigin="anonymous"></script>
 </body>
 </html>
